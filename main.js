bc0a5a08fb83262763ba69be6ae2146f550353dc
furkan-developer <furknn2112@gmail.com>
2024-04-19 18:09:18 +0300

    promise approach using await and async with fetch method that return promise

--- main.js
+++ main.js
@@ -2,80 +2,40 @@ import { default as UI } from "./Modules/UI.js";
 const ui = new UI();
 const url = "https://restcountries.com/v3.1/";
 
-ui.searchBtn.addEventListener("click", () => {
+ui.searchBtn.addEventListener("click", async () => {
     let countryName = ui.takeCountryName();
 
     if (countryName !== "") {
-        let request = makeRequest(`name/${countryName}`, "GET");
+        const response = await fetch(`${url}name/${countryName}`);
 
-        request
-            .then((xhr) => okCountry(xhr))
-            .catch(err => console.log(err));
+        if (!response.ok) {
+            notFoundCountry();
+            return;
+        }
+
+        let country = await response.json();
+        okCountry(country);
+
+        if (!country[0].hasOwnProperty("borders")) {
+            ui.neighborsContent.innerHTML = `
+                        <p>The country has not neighbor</p>
+                    `
+        }
+        else {
+            let response = await fetch(`${url}alpha?codes=${country[0].borders.toString()}`);
+            let neighbors = await response.json();
+            console.log(neighbors);
+            okNeighbors(neighbors);
+        }
     }
 });
 
-function makeRequest(uri, method) {
-    // Create the XHR request
-    const xhr = new XMLHttpRequest();
+const okCountry = function (countryInfo) {
 
-    // Return it as a Promise
-    return new Promise(function (resolve, reject) {
-        // Setup our listener to process compeleted requests
-        xhr.onreadystatechange = function () {
+    if (ui.countryDetailsWrapper.classList.contains("invisible"))
+        ui.countryDetailsWrapper.classList.remove("invisible");
 
-            // Only run if the request is complete
-            if (xhr.readyState !== 4) return;
-
-            // Process the response
-            if (xhr.status >= 200 && xhr.status < 500) {
-                // If successful
-                resolve(xhr);
-            } else {
-                // If failed
-                reject({
-                    status: xhr.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-
-        // Setup our HTTP request
-        xhr.open(method || 'GET', url + uri, true);
-
-        // Send the request
-        xhr.send();
-    });
-};
-
-const okCountry = function (xhr) {
-
-    if (xhr.status == 200) {
-        if (ui.countryDetailsWrapper.classList.contains("invisible"))
-            ui.countryDetailsWrapper.classList.remove("invisible");
-
-        let countryInfo = JSON.parse(xhr.responseText);
-
-        ui.countryDetailsContent.innerHTML = ui.renderCountryDetails(countryInfo[0])
-
-        if (!countryInfo[0].hasOwnProperty("borders")) {
-            ui.neighborsContent.innerHTML = `
-                    <p>The country has not neighbor</p>
-                `
-        }
-
-        else {
-            // Perform request for corrosponding country's neighbors
-            let request = makeRequest(`alpha?codes=${countryInfo[0].borders.toString()}`,"GET");
-
-            request
-                .then(xhr => {
-                    if(xhr.status == 200) okNeighbors(xhr);
-                })
-                .catch(err => console.log(err));
-        }
-    }
-    else if (xhr.status == 404)
-        notFoundCountry();
+    ui.countryDetailsContent.innerHTML = ui.renderCountryDetails(countryInfo[0])
 }
 
 const notFoundCountry = function () {
@@ -88,8 +48,6 @@ const notFoundCountry = function () {
     ui.neighborsContent.innerHTML = "";
 }
 
-const okNeighbors = function (xhr) {
-    let neighborsInfo = JSON.parse(xhr.responseText);
-
+const okNeighbors = function (neighborsInfo) {
     ui.neighborsContent.innerHTML = ui.renderNeighbors(neighborsInfo);
 }
