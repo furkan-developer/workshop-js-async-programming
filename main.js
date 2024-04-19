a8cb30f8bfda0a222641c01c4327c615a0d20bff
furkan-developer <furknn2112@gmail.com>
2024-04-19 17:16:28 +0300

    promise approach using then, catch and finally method

--- main.js
+++ main.js
@@ -1,45 +1,81 @@
 import { default as UI } from "./Modules/UI.js";
 const ui = new UI();
+const url = "https://restcountries.com/v3.1/";
 
 ui.searchBtn.addEventListener("click", () => {
     let countryName = ui.takeCountryName();
 
     if (countryName !== "") {
-        const xhttp = new XMLHttpRequest();
-        xhttp.open("GET", `https://restcountries.com/v3.1/name/${countryName}`);
-        xhttp.send();
+        let request = makeRequest(`name/${countryName}`, "GET");
 
-        // the callback function that will execute when response is ready.
-        xhttp.onload = function () {
-            operateBehavior(this, { "200": okCountry, "404": notFoundCountry });
-        }
+        request
+            .then((xhr) => okCountry(xhr))
+            .catch(err => console.log(err));
     }
 });
 
+function makeRequest(uri, method) {
+    // Create the XHR request
+    const xhr = new XMLHttpRequest();
+
+    // Return it as a Promise
+    return new Promise(function (resolve, reject) {
+        // Setup our listener to process compeleted requests
+        xhr.onreadystatechange = function () {
+
+            // Only run if the request is complete
+            if (xhr.readyState !== 4) return;
+
+            // Process the response
+            if (xhr.status >= 200 && xhr.status < 500) {
+                // If successful
+                resolve(xhr);
+            } else {
+                // If failed
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+
+        // Setup our HTTP request
+        xhr.open(method || 'GET', url + uri, true);
+
+        // Send the request
+        xhr.send();
+    });
+};
+
 const okCountry = function (xhr) {
-    if (ui.countryDetailsWrapper.classList.contains("invisible"))
-        ui.countryDetailsWrapper.classList.remove("invisible");
 
-    let countryInfo = JSON.parse(xhr.responseText);
+    if (xhr.status == 200) {
+        if (ui.countryDetailsWrapper.classList.contains("invisible"))
+            ui.countryDetailsWrapper.classList.remove("invisible");
 
-    ui.countryDetailsContent.innerHTML = ui.renderCountryDetails(countryInfo[0])
+        let countryInfo = JSON.parse(xhr.responseText);
 
-    if (!countryInfo[0].hasOwnProperty("borders")) {
-        ui.neighborsContent.innerHTML = `
+        ui.countryDetailsContent.innerHTML = ui.renderCountryDetails(countryInfo[0])
+
+        if (!countryInfo[0].hasOwnProperty("borders")) {
+            ui.neighborsContent.innerHTML = `
                     <p>The country has not neighbor</p>
                 `
-        return
-    }
+        }
 
-    // Perform request for corrosponding country's neighbors
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `https://restcountries.com/v3.1/alpha?codes=${countryInfo[0].borders.toString()}`);
-    xhttp.send();
+        else {
+            // Perform request for corrosponding country's neighbors
+            let request = makeRequest(`alpha?codes=${countryInfo[0].borders.toString()}`,"GET");
 
-    // the callback function that will execute when response is ready.
-    xhttp.onload = function () {
-        operateBehavior(this, { "200": okNeighbors, "404": notFoundCountry })
+            request
+                .then(xhr => {
+                    if(xhr.status == 200) okNeighbors(xhr);
+                })
+                .catch(err => console.log(err));
+        }
     }
+    else if (xhr.status == 404)
+        notFoundCountry();
 }
 
 const notFoundCountry = function () {
@@ -56,23 +92,4 @@ const okNeighbors = function (xhr) {
     let neighborsInfo = JSON.parse(xhr.responseText);
 
     ui.neighborsContent.innerHTML = ui.renderNeighbors(neighborsInfo);
-}
-
-function operateBehavior(xhr, statusBehaviors) {
-    switch (xhr.status) {
-        case 200:
-            if (hasStatusBehavior(statusBehaviors, 200))
-                statusBehaviors["200"](xhr);
-            break;
-        case 404:
-            if (hasStatusBehavior(statusBehaviors, 400))
-                statusBehaviors["404"]();
-            break;
-        default:
-            throw "Not implemented exception for status code specified";
-    }
-}
-
-function hasStatusBehavior(object, propertyName) {
-    return object.hasOwnProperty(propertyName) ? true : () => { throw `Not implemented exception for ${propertyName} status code`; }
 }
